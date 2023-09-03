eaa90d3bbf449c8640b46f0554ceabaa60f7f755
GER-ITL <danteryzvelt@inbox.ru>
2023-09-03 09:48:07 +0300

    proj 1-2

--- Project 2/app.js
+++ Project 2/app.js
@@ -3,16 +3,76 @@ const createBtn = document.getElementById('create')
 const listElement = document.getElementById('list')
 //console.log(inputElement.value)
 
-const notes = ['1', '2']
-function render(){
-    listElement.insertAdjacentHTML('beforeend', `  <div><li class = 'liList'>
-<span >${inputElement.value}</span>
-    <div class= 'divBtn'>
-        <span class='Btn' id='add'>✔️</span>
-        <span class='Btn' id='del'>❌</span>
+//const notes = ['Заметка 1', 'Заметка 2']
+/*function render(){
+    for(let i = 0; i < notes.length; i++){
+        listElement.insertAdjacentHTML('beforeend', getNoteTemp(notes[i]))
+    }
+
+}
+
+render()*/
+createBtn.onclick = function(){
+    if(inputElement.value.length == 0 ){
+        return 
+    }
+/*listElement.innerHTML = `  <li>
+    <label>${inputElement.value}</label>
+        <div class="btnDiv">
+            <span id='add'>✔️</span>
+            <span id='del'>❌</span>
         </div>
-    </li>
-</div>`)
+</li>`*/
+
+const newNote ={
+    title: inputElement.value,
+    completed: false,
+}
+listElement.insertAdjacentHTML('beforeend',getNoteTemp(newNote))
+inputElement.value = ''
+}
+
+/*const notes= [{
+    title: 'Заметка 1',
+    completed: false,
+},
+{
+    title: 'Заметка 2',
+    completed: true,
+},
+]*/
+function render(){
+    for(let i = 0; i < notes.length; i++){
+        listElement.insertAdjacentHTML('beforeend', getNoteTemp(notes[i]))
+    }
+
+}
+
+render()
+
+function getNoteTemp(note){
+    return `  <div><li class = 'liList'>
+    <span class="${note.completed ? 'text-decoration-line-through' : ''}">${note.title}</span>
+        <div class= 'divBtn'>
+            <span class='Btn-${note.completed ? 'warning' : 'success'}' id='add'>✔️</span>
+            <button class='Btn' id='del'>❌</button>
+            </div>
+        </li>
+    </div>`
+}
+
+
+/*
+
+
+const inputElement = document.getElementById('title')
+const createBtn = document.getElementById('create')
+const listElement = document.getElementById('list')
+//console.log(inputElement.value)
+
+/*function render(){
+    listElement.insertAdjacentHTML('beforeend', getNoteTemp(notes[1]))
+    listElement.insertAdjacentHTML('beforeend', getNoteTemp(notes[0]))
 }
 
 createBtn.onclick = function(){
@@ -25,14 +85,36 @@ createBtn.onclick = function(){
             <span id='add'>✔️</span>
             <span id='del'>❌</span>
         </div>
-</li>`*/
-listElement.insertAdjacentHTML('beforeend', `  <div><li class = 'liList'>
-<span >${inputElement.value}</span>
-    <div class= 'divBtn'>
-        <span class='Btn' id='add'>✔️</span>
-        <span class='Btn' id='del'>❌</span>
-        </div>
-    </li>
-</div>`)
+</li>`
+listElement.insertAdjacentHTML('beforeend', getNoteTemp(inputElement.value))
 inputElement.value = ''
 }
+
+function getNoteTemp(note){
+    return `  <div><li class = 'liList'>
+    <span >${inputElement.value}</span>
+        <div class= 'divBtn'>
+            <span class='Btn' id='add'>✔️</span>
+            <span class='Btn' id='del'>❌</span>
+            </div>
+        </li>
+    </div>`
+}
+const notes= [{
+    title: 'Заметка 1',
+    completed: false,
+},
+{
+    title: 'Заметка 2',
+    completed: true,
+}
+]
+
+function render(){
+    for(let i = 0 ; i < notes.length; i++){
+        listElement.insertAdjacentHTML('beforeend', getNoteTemp(notes))
+    }
+}
+render()
+ */
+
