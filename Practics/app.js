1cfbfb3f545b6918d70d108048e5a2d9ca721075
GER-ITL <danteryzvelt@inbox.ru>
2023-09-06 17:36:22 +0300

    finally

---
+++ Practics/app.js
@@ -0,0 +1,22 @@
+/*function getRandom(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min) 
+}
+console.log(getRandom(0, 10))*/
+const resultElement = document.getElementById('result')
+const input1 = document.getElementById('input1')
+const submitBtn = document.getElementById('submit')
+
+let inputNum = Number(input1.value)
+const randomNum = function getRandom(min, max){
+    const randomNum =  Math.floor(Math.random() * (max - min + 1) + min)
+}
+function rez (){
+if (inputNum == randomNum) {
+    resultElement.textContent = 'Вы угадали!!!'
+}  else if (inputNum < randomNum){ 
+    resultElement.textContent = 'Введите число побольше'
+}else if (inputNum > randomNum){ 
+    resultElement.textContent = 'Введите число поменьше'
+}
+}
+submitBtn.onclick = function(){console.log(randomNum(1,100))}
