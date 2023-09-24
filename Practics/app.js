dd826205608f67b1eeb46e116d4c8555f9b29957
GER-ITL <danteryzvelt@inbox.ru>
2023-09-24 18:52:58 +0300

    jspr

--- Practics/app.js
+++ Practics/app.js
@@ -2,21 +2,23 @@
     return Math.floor(Math.random() * (max - min + 1) + min) 
 }
 console.log(getRandom(0, 10))*/
-const resultElement = document.getElementById('result')
-const input1 = document.getElementById('input1')
-const submitBtn = document.getElementById('submit')
+// const resultElement = document.getElementById('result')
+// const input1 = document.getElementById('input1')
+// const submitBtn = document.getElementById('submit')
 
-let inputNum = Number(input1.value)
-const randomNum = function getRandom(min, max){
-    const randomNum =  Math.floor(Math.random() * (max - min + 1) + min)
-}
-function rez (){
-if (inputNum == randomNum) {
-    resultElement.textContent = 'Вы угадали!!!'
-}  else if (inputNum < randomNum){ 
-    resultElement.textContent = 'Введите число побольше'
-}else if (inputNum > randomNum){ 
-    resultElement.textContent = 'Введите число поменьше'
-}
-}
-submitBtn.onclick = function(){console.log(randomNum(1,100))}
+// let inputNum = Number(input1.value)
+// const randomNum = function getRandom(min, max){
+//     const randomNum =  Math.floor(Math.random() * (max - min + 1) + min)
+// }
+// function rez (){
+// if (inputNum == randomNum) {
+//     resultElement.textContent = 'Вы угадали!!!'
+// }  else if (inputNum < randomNum){ 
+//     resultElement.textContent = 'Введите число побольше'
+// }else if (inputNum > randomNum){ 
+//     resultElement.textContent = 'Введите число поменьше'
+// }
+// }
+// submitBtn.onclick = function(){console.log(randomNum(1,100))}
+
+
